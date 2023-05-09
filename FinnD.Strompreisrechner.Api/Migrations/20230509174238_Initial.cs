18b7aba60377888c8e232cb0771068ee4166ec8b
Finn <finn1999.duehrkop@gmail.com>
2023-05-09 20:20:45 +0200

    add migration

---
+++ FinnD.Strompreisrechner.Api/Migrations/20230509174238_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinnD.Strompreisrechner.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "history_entries",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    kwh_per_year = table.Column<decimal>(type: "TEXT", nullable: false),
+                    price_per_kwh = table.Column<decimal>(type: "TEXT", nullable: false),
+                    price_per_year = table.Column<decimal>(type: "TEXT", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "datetime('now')")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_history_entries", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "history_entries");
+        }
+    }
+}
